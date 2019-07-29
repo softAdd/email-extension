eea1fb472ea4e016cae67b0510fc0d209417029b
softAdd <soft.snake867@gmail.com>
2019-07-29 20:44:58 +0500

    first commit

---
+++ insert_text.js
@@ -0,0 +1,4 @@
+console.log('1')
+// console.log(window.getSelection().toString())
+console.log(window.document.activeElement)
+window.document.activeElement.value += '@textvalue';
---
+++ src/save_text.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+    const storage = chrome.storage.sync;
+    const input = document.querySelector('.add-input');
+    const button = document.querySelector('.add-button');
+    const list = document.querySelector('.text-list');
+
+    let lines = [];
+
+    storage.get(['lines'], function (result) {
+        if (!Array.isArray(result['lines'])) {
+            storage.set({ 'lines': [] });
+        } else {
+            lines = [...result['lines']];
+            lines.forEach((str, index) => {
+                addItem(list, str, index + 1);
+            });
+        }
+    })
+
+    button.addEventListener('click', function () {
+        const text = input.value;
+        input.value = '';
+        input.focus();
+        lines = [...lines, text];
+        addItem(list, lines[lines.length - 1], lines.length);
+        storage.set({ 'lines': lines });
+    });
+
+    input.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13) {
+            button.dispatchEvent(new Event('click'));
+        }
+    });
+}
+
+function addItem(elem, text, index) {
+    elem.innerHTML += `<br/>${index}. ${text}`;
+}
