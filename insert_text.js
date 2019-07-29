0da8009ac93294f0a1705d16171137d1bc08ee29
softAdd <soft.snake867@gmail.com>
2019-07-29 21:01:30 +0500

    storage

--- insert_text.js
+++ insert_text.js
@@ -1,4 +1,8 @@
 // console.log('1')
 // // console.log(window.getSelection().toString())
 // console.log(window.document.activeElement)
+const storage = chrome.storage.sync;
 window.document.activeElement.value += '123';
+storage.get(['lines'], function(result) {
+    alert(result['lines'])
+})
