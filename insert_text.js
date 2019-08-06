54ffdaaae6270e1cd333323fc8462bb45a1d56e5
softAdd <soft.snake867@gmail.com>
2019-08-06 19:18:52 +0500

    minimum of prefix

--- insert_text.js
+++ insert_text.js
@@ -1,6 +1,12 @@
 (async function() {
-    const currentText = await recieveData('currentText');
-    const elem = window.document.activeElement;
+    let currentText = await recieveData('currentText');
+    let userPrefix = await recieveData('prefix');
+    let currentDomain = await recieveData('currentDomain');
+    let currentUrl = currentText.split('@')[0];
+    if (userPrefix !== undefined && userPrefix !== '') {
+        currentText = `${userPrefix}+${currentUrl}${currentDomain}`;
+    }
+    let elem = window.document.activeElement;
         if (elem.value === undefined) {
             elem.innerHTML += currentText;
         } else {
